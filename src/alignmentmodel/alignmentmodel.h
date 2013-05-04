e430000b56a7024c0464535cadcd42707c132fc5
Michael Nokel <mnokel@gmail.com>
2013-05-04 19:51:32 +0400

    Alignment model

--- src/alignmentmodel/alignmentmodel.h
+++ src/alignmentmodel/alignmentmodel.h
@@ -1,11 +1,19 @@
 #ifndef ALIGNMENTMODEL_ALIGNMENTMODEL_H
 #define ALIGNMENTMODEL_ALIGNMENTMODEL_H
 
-#include "../phrasetable/phrasetable.h"
+#include <cmath>
 
 class AlignmentModel {
  public:
-  double get_probabilty(int distance);
+  AlignmentModel(double alpha=0.5) : alpha_(alpha) {
+  }
+
+  double get_probabilty(int distance) {
+    return pow(alpha_, fabs(distance));
+  }
+
+ private:
+  double alpha_;
 };
 
 #endif // ALIGNMENTMODEL_ALIGNMENTMODEL_H
