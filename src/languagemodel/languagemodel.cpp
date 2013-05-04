484fcf24c865d5a5daf49ec01105ad71c0b04311
supremus <dmitry.marin@gmail.com>
2013-05-04 23:00:44 +0600

    language model load/save interface implementation

--- src/languagemodel/languagemodel.cpp
+++ src/languagemodel/languagemodel.cpp
@@ -2,6 +2,7 @@
 #define LANGUAGEMODEL_LANGUAGEMODEL_H
 
 #include <unordered_map>
+#include <fstream>
 
 #include "languagemodel.h"
 
@@ -21,6 +22,75 @@ double NgramLanguageModel::get_context_count(Phrase phrase) const {
 	phrase.pop_back();
 	return mapAtOrDefault(seenContext_, phrase, 0);
 }
+
+template< class T >
+static void write(const T& t, FILE* output) {
+	fwrite(&t, sizeof(t), 1, output);
+}
+
+static void writePhraseMap(const NgramLanguageModel::Phrase2Stat& map, FILE* output) {
+	write(map.size(), output);
+	for(auto& phraseStat : map) {
+		auto& phrase = phraseStat.first;
+		write(phrase.size(), output);
+		for (Word w : phrase)
+			write(w, output);
+		write(phraseStat.second, output);
+	}
+}
+
+template< class T >
+static T read(FILE* input) {
+	T result;
+	fread(&result, sizeof(result), 1, input);
+	return result;
+}
+
+static NgramLanguageModel::Phrase2Stat readPhraseMap(FILE* input) {
+	NgramLanguageModel::Phrase2Stat result;
+	const size_t count = read< size_t >(input);
+	for (size_t i = 0; i < count; ++i) {
+		const size_t phraseLength = read< size_t >(input);
+		Phrase phrase;
+		phrase.reserve(phraseLength);
+		for (size_t j = 0; j < phraseLength; ++j) {
+			phrase.push_back(read< Word >(input));
+		}
+		result[phrase] = read< NgramLanguageModel::Phrase2Stat::mapped_type >(input);
+	}
+	return result;
+}
+
+void NgramLanguageModel::save(const std::string& path) const {
+	FILE* output = fopen(path.c_str(), "wb");
+	write(N_, output);
+	write(alpha_, output);
+	write(nu_, output);
+	writePhraseMap(seenPhrases_, output);
+	writePhraseMap(seenContext_, output);
+	fclose(output);
+}
+
+bool NgramLanguageModel::operator ==(const NgramLanguageModel& other) const {
+	return seenContext_ == other.seenContext_ 
+		&& seenPhrases_ == other.seenPhrases_
+		&& N_ == other.N_
+		&& alpha_ == other.alpha_
+		&& nu_ == other.nu_;
+}
+
+
+NgramLanguageModel load_ngram_language_model(const std::string& path) {
+	FILE* input = fopen(path.c_str(), "rb");
+	size_t N = read< size_t >(input);
+	double alpha = read< double >(input);
+	double nu = read< double >(input);
+	auto seenPhrases = readPhraseMap(input);
+	auto seenContext = readPhraseMap(input);
+	fclose(input);
+	return NgramLanguageModel(std::move(seenPhrases), std::move(seenContext), N, alpha, nu);
+}
+
 
 NgramLanguageModel learn_ngram_language_model(const std::vector< Phrase >& sentences, const int wordsCount, const size_t N, const double alpha) {
 	const double nu = wordsCount;
