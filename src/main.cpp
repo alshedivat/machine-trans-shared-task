6c701bbfa8f4b181aa87a2753d07428490aef40e
Michael Nokel <mnokel@gmail.com>
2013-05-04 21:14:01 +0400

    Fixes in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iostream>
 #include <stdexcept>
 #include "alignmentmodel/alignmentmodel.h"
 #include "converter/converter.h"
@@ -6,6 +7,7 @@
 #include "languagemodel/languagemodel.h"
 #include "phrasetable/phrasetable.h"
 
+using std::cerr;
 using std::cin;
 using std::cout;
 using std::getline;
@@ -28,7 +30,7 @@ LanguageModel loadLanguageModel(Converter& converter) {
     string sentence;
     vector<Phrase> sentences;
     while (getline(file, sentence)) {
-        sentences.push_back(Converter.ToIndex(sentence));
+        sentences.push_back(converter.ToIndex(sentence));
     }
     file.close();
     return learn_ngram_language_model(sentences, converter.DictSize());
@@ -38,7 +40,7 @@ int main() {
     try {
         Converter english_converter(ENGLISH_VOCABULARY_PATH);
         Converter french_converter(FRENCH_VOCABULARY_PATH);
-        LanguageModel languageModel = loadLanguageModel(english_converter);
+        LanguageModel language_model = loadLanguageModel(english_converter);
         AlignmentModel alignment_model;
         PhraseTable phrase_table = load_phrase_table(PHRASE_TABLE_PATH);
         Decoder decoder(language_model, alignment_model, phrase_table);
@@ -46,7 +48,7 @@ int main() {
         while (getline(cin, sentence)) {
             Phrase french_phrase = french_converter.ToIndex(sentence);
             Phrase english_phrase = decoder.decode(french_phrase);
-            cout >> english_converter.ToSentence(english_phrase);
+            cout << english_converter.ToSentence(english_phrase) << endl;
         }
         return 0;
     } catch (const exception& error) {
