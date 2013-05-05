0a9dbcce3e742b61c4ae265fa5ae93a547994fca
supremus <dmitry.marin@gmail.com>
2013-05-05 21:48:21 +0600

    fix: language model

--- src/languagemodel/languagemodel.h
+++ src/languagemodel/languagemodel.h
@@ -22,17 +22,18 @@ public:
 	{
 	}
 
+	/// returns probability of whole sentence
 	double get_probability(const Phrase& sentence) const {
 		if (sentence.size() < 1)
 			throw std::runtime_error("wrong phrase length");
-		Phrase phrase(N_ - 1, 0);
-		phrase.insert(phrase.end(), sentence.begin(), sentence.end());
+		Phrase phrase;
 		double language_model_cost = 0;
-		for (size_t index = 0; index + 3 <= phrase.size(); ++index) {
-			Phrase subphrase = Phrase(phrase.begin() + index, phrase.begin() + index + 3);
-
-			double num = get_count(subphrase);
-			double denum = get_context_count(subphrase);
+		for (Word word : sentence) {
+			phrase.push_back(word);
+			if (phrase.size() > get_length())
+				phrase.erase(phrase.begin());
+			double num = get_count(phrase);
+			double denum = get_context_count(phrase);
 			language_model_cost += log(num + alpha_) - log(denum + nu_ * alpha_);
 		}
 		return language_model_cost;
