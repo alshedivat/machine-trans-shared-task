3166b0500531af3f730d506e5a15f5a87c86d4e2
Maruan <alshedivat.maruan@gmail.com>
2013-05-06 15:48:02 +0400

    Added reference google translation for the test data

--- src/preprocessing/preprocess_phrase_table.sh
+++ src/preprocessing/preprocess_phrase_table.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gzip -cd ../../data/shad-shared-task/training/model/phrase-table.gz |\
+gzip -cd ../../data/moses-training/model/phrase-table.gz |\
 ./preprocess_phrase_table.py ../../data/corpus/en.vcb ../../data/corpus/fr.vcb |\
 gzip -c > ../../data/corpus/phrase_table.gz
