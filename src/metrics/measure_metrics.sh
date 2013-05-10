c23fa2d3e9fe69ac7beddb37d0de82140bd5aa14
Maruan <alshedivat.maruan@gmail.com>
2013-05-10 21:03:04 +0400

    Added Bing reference set

--- src/metrics/measure_metrics.sh
+++ src/metrics/measure_metrics.sh
@@ -1,2 +1,4 @@
-./mteval.pl -s ../../data/corpus/input.xml -r ../../data/corpus/input_google_reference.xml -t ../../data/corpus/output.xml
-./mteval.pl -s ../../data/corpus/input.xml -r ../../data/corpus/input_yandex_reference.xml -t ../../data/corpus/output.xml
+#!/bin/bash
+./mteval.pl -s ../../data/corpus/input.xml -r ../../data/corpus/input_google_reference.xml -t ../../data/corpus/output.xml -d
+./mteval.pl -s ../../data/corpus/input.xml -r ../../data/corpus/input_yandex_reference.xml -t ../../data/corpus/output.xml -d
+./mteval.pl -s ../../data/corpus/input.xml -r ../../data/corpus/input_bing_reference.xml -t ../../data/corpus/output.xml -d
--- src/preprocessing/preprocess_output.sh
+++ src/preprocessing/preprocess_output.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 pv ../../data/corpus/output.txt | ./data_to_xml.py > ../../data/corpus/output.xml
